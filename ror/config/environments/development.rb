12c08d9d1dac688a572cc6d22a996d8eb3228cff
simba <dan.jebamony@gmail.com>
2024-10-30 01:40:55 +0800

    Fix mailer

--- ror/config/environments/development.rb
+++ ror/config/environments/development.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   # Set default URL options
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-  config.action_mailer.preview_path = "#{Rails.root}/test/mailers/previews"
+  config.action_mailer.preview_paths << "#{Rails.root}/test/mailers/previews"
 end
